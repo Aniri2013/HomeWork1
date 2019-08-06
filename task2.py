decabcef786a846caec6b453454c2b36d047768d
Aniri2013 <aniri-13.rodina@yandex.ru>
2019-08-06 22:06:01 +0300

    commit

--- task2.py
+++ task2.py
@@ -3,5 +3,3 @@ T1 = float(input("Enter T1"))
 R2 = float(input("Enter R2"))
 T2 = (T1**2 * R2** 3 / R1**3)**0.5
 print ('T2 =', T2)
-
-
---
+++ task5.py
@@ -0,0 +1,17 @@
+xA = float(input("Enter xA: "))
+yA = float(input("Enter yA: "))
+
+xB = float(input("Enter xB: "))
+yB = float(input("Enter yB: "))
+
+xC = float(input("Enter xC: "))
+yC = float(input("Enter yC: "))
+
+xD = float(input("Enter xD: "))
+yD = float(input("Enter yD: "))
+
+S = abs((xB - xA)*(yD - yA) - (yB - yA)*(xD - xA))
+D = ((xA - xС)**2 - (yA - yC)**2) ** 0.5
+D2 = ((xB - xD)**2 - (yB - yD)**2) ** 0.5
+print("S = ", S)
+print("D = ", D)
